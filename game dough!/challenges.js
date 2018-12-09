e4402c0549f6a3b3946943c470690eb86e369ac3
agnava <42649480+agnava@users.noreply.github.com>
2018-12-09 12:10:51 +0530

    MADE MY FIRST JAVASCRIPT GAME

---
+++ game dough!/challenges.js
@@ -0,0 +1,124 @@
+
+
+var scores,roundScore,activePlayer,gamePlaying;
+
+init();
+
+var lastDice;
+
+document.querySelector(".dice").style.display ="none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+
+
+document.querySelector(".btn-roll").addEventListener("click",function(){
+    
+    if(gamePlaying){
+
+         //Random NO.
+    dice = Math.floor(Math.random()* 6) +1 ;
+    //Display the result
+    var diceDom = document.querySelector(".dice");
+    diceDom.style.display = "block";
+    diceDom.src = "dice-"+ dice + ".png";
+    //Update the round score if the rolled no was not  a 1.
+    if(dice===6&&lastDice===6){
+        //player loses score
+        scores[activePlayer]=0;
+        document.querySelector("#score-"+activePlayer).textContent ="0";
+        nextPlayer();
+
+    }
+    else if(dice !== 1){
+        //add score
+        roundScore += dice;
+        document.querySelector("#current-"+ activePlayer).textContent = roundScore;
+    }
+    else{
+        ////next playr
+
+       nextPlayer();
+    }
+    var lastDice = dice;
+    }
+    
+});
+//hold
+document.querySelector(".btn-hold").addEventListener("click",function(){
+    if(gamePlaying){
+        //add current score to Global score
+    scores[activePlayer]+= roundScore;
+
+    //update the UI
+    document.querySelector("#score-"+activePlayer).textContent =scores[activePlayer];
+        var input = document.querySelector(".final-score").value;
+        var winningScore;
+        if(input){
+         winningScore = input;
+        }
+        else{
+            winningScore = 100;
+        }
+    //check if player won the game
+    if(scores[activePlayer]>=100){
+        document.querySelector("#name-"+ activePlayer).textContent="winner";
+        document.querySelector(".dice").style.display = "none";
+        document.querySelector(".player-"+ activePlayer+ "-panel").classList.add("winner");
+        document.querySelector(".player-"+ activePlayer+ "-panel").classList.remove("active");
+        gamePlaying= false;
+    }
+    else{
+        nextPlayer();
+    }
+    }
+    
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer =1 : activePlayer=0;
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+
+    document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click",init);
+
+function init(){
+    scores=[0,0];
+roundScore = 0;
+activePlayer = 0;
+gamePlaying = true;
+
+document.querySelector(".dice").style.display ="none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+document.getElementById("name-1").textContent="Player 2";
+document.getElementById("name-0").textContent="Player 1";
+document.querySelector(".player-0-panel").classList.remove("winner");
+document.querySelector(".player-1-panel").classList.remove("winner");
+document.querySelector(".player-0-panel").classList.remove("active");
+document.querySelector(".player-1-panel").classList.remove("winner");
+document.querySelector(".player-0-panel").classList.add("active");
+}
+
+
+
+
+
+
+
+
